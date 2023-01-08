3c5861af7b5472917e94b9a1e114e696e8d7636a
Tra Nguyen <nstlegend@gmail.com>
2023-01-08 17:54:43 +0800

    Create initial introduction page

---
+++ src/stories/introduction.tsx
@@ -0,0 +1,101 @@
+import { Title } from 'components/title/title';
+import { Text } from 'components/text/text';
+import { Input } from 'components/input/input';
+import { Select } from 'components/select/select';
+import { CheckboxGroup } from 'components/checkbox-group/checkbox-group';
+import { RadioGroup } from 'components/radio-group/radio-group';
+import { Card } from 'components/card/card';
+import { Button } from 'components/button/button';
+import { Section } from 'components/section/section';
+import { Link } from 'components/link/link';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+const GENDER = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+];
+
+const PERFERENCE = [
+  { label: 'Books', value: 'book' },
+  { label: 'eBooks', value: 'ebook' },
+  { label: 'Audio books', value: 'audio' },
+  { label: 'Journals', value: 'journal' },
+  { label: 'Newspapers', value: 'news' }
+];
+
+
+export  const Introduction = () => {
+  return (
+    <>
+      <Title
+        size='large'
+      >
+        LUNAR - A Superellipse React UI Kit
+      </Title>
+
+      <Text>
+        Superellipse is a germetric shape resembles a transition between a rectangle and a circle. You might have seen its special case (i.e. a squircle) as an app icon on iOS.
+      </Text>
+
+      <Text>
+        Please go through each component's stories for their detailed usage. Here is an overview of LUNAR.
+      </Text>
+
+      <Title>
+        The Principles Behind LUNAR
+      </Title>
+
+      <Text>
+        LUNAR simulates superellipses, resulting in more natural curves compared to the native rounded rectangles. At the same time, LUNAR utilises human-computer interaction principles to produce clean and easy-to-use UI components.
+      </Text>
+
+      <Title>
+        Component Showcase
+      </Title>
+
+      <Row>
+        <Col xs>
+          <Card
+            label='A Form That Reponses To User Action'
+          >
+            <Text>
+              "Fillable" areas are subtly highlighted.
+            </Text>
+            <Input
+              label='Full Name'
+            />
+            <RadioGroup
+              label='Gender'
+              options={GENDER}
+            />
+            <CheckboxGroup
+              label='Favorite Content'
+              options={PERFERENCE}
+            />
+            <Row>
+              <Col 
+                xs
+              >
+                <Button
+                  label='Reset'
+                />
+              </Col>
+              <Col 
+                xs 
+                style={{ textAlign: 'right' }}
+              >
+                <Button
+                  primary
+                  label='Submit'
+                />
+              </Col>
+
+            </Row>
+          </Card>
+        </Col>
+        <Col xs>
+        </Col>
+      </Row>
+    </>
+  );
+};
